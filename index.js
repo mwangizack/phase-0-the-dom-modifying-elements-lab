9194ab2ef8ed3919e3f2973bd9c211bb242d0a1c
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-04-10 14:36:15 +0300

    Manipulate the DOM to remove the main element and add a h1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.querySelector("#main");
+main.remove();
+
+let newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader = document.querySelector("h1");
+newHeader.id = "victory";
+newHeader.textContent = 'Zack is the champion'
